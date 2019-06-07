4075f142ea7d8377e1e2c706f3c088e500d8e0b3
yyfann <a1097833559@163.com>
2019-06-07 20:34:24 +0800

    shoot-game

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const http = require('http')
-
+const fs = require('fs')
 http.createServer((req, res) => {
-  res.end('8080')
+
+  console.log(req.url,'req.url')
+  if (req.url === '/') {
+    const shootGame = fs.readFileSync('./src/shoot-game.html', 'utf-8')
+    res.end(shootGame)
+  }
+
+  if (req.url === '/l1.png') {
+    console.log(33,'33')
+    const l1 = fs.readFileSync('./src/l1.png')
+    console.log(l1,'l1')
+    res.end(l1)
+  }
 }).listen(8080, () => {
   console.log('server is running on 8080')
 })
